67111dc9ef96df194d9d9caf919518d1ed0f59c6
Andhikap <prameswaara@gmail.com>
2023-11-17 21:27:45 +0700

    do feature delete by id

--- student-form-example/src/main/java/com/prameswaradev/demo/controller/StudentController.java
+++ student-form-example/src/main/java/com/prameswaradev/demo/controller/StudentController.java
@@ -1,2 +1,62 @@
-package com.prameswaradev.demo.controller;public class StudentController {
+package com.prameswaradev.demo.controller;
+
+import com.prameswaradev.demo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class StudentController {
+    List<Student> students = new ArrayList<>();
+
+    @GetMapping("/")
+    public String form(Model model, @RequestParam(required = false) UUID id){
+        Student student = isExist(id);
+        model.addAttribute("student", student != null ? student : new Student() );
+        return "form";
+    }
+
+    private Student isExist(UUID id) {
+        return students.stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping("/submit")
+    public String submit(Student student){
+        Student studentFound = isExist(student.getId());
+        if (studentFound == null){
+            student.setId(UUID.randomUUID());
+            students.add(student);
+        } else {
+            students.remove(studentFound);
+            students.add(student);
+        }
+        return "redirect:/students";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam UUID id){
+        Student studentFound = isExist(id);
+        if (studentFound != null){
+            students.remove(studentFound);
+        }
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students")
+    public String getStudents(Model model){
+        model.addAttribute("students",  students);
+        return "students";
+    }
+
+
 }
--- student-form-example/src/main/java/com/prameswaradev/demo/model/Student.java
+++ student-form-example/src/main/java/com/prameswaradev/demo/model/Student.java
@@ -1,2 +1,20 @@
-package com.prameswaradev.demo.model;public class Student {
+package com.prameswaradev.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student{
+    private UUID id;
+    private String name;
+    private String major;
+
+    public Student() {
+
+    }
 }
